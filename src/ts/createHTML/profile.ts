ca38e1eff155d70c1b602181c00532f6012b74cd
Leonard Deutgen <leonard.deutgen@medieinstitutet.se>
2024-01-25 14:05:50 +0100

    :bulb: Added comment and added pop method

--- src/ts/createHTML/profile.ts
+++ src/ts/createHTML/profile.ts
@@ -1,42 +1,43 @@
-import { dlcVideos } from "../main"
+import { dlcVideos } from "../main";
 import { ISnippet } from "../models/ISnippet";
 
 const profileContainer = document.getElementById("container--profile");
 const navBar = document.getElementById("navBar");
 
-export let profileList:ISnippet[] = [];
+export let profileList: ISnippet[] = [];
 const LSValue = localStorage.getItem("profileList");
 
-if(LSValue){
-    profileList = JSON.parse(LSValue);
+if (LSValue) {
+  profileList = JSON.parse(LSValue);
 }
 
-export const createHTMLProfile = ()=>{
-    localStorage.setItem("profileList", JSON.stringify(profileList));
+export const createHTMLProfile = () => {
+  localStorage.setItem("profileList", JSON.stringify(profileList));
 
-    for (let i=0; i<profileList.length; i++){
-        const profileImg = document.createElement("img");
-        const profileTitle = document.createElement("h2");
-        const profileTextBox = document.createElement("div");
-        const profileText = document.createElement("p");
+  //Tömmer sidan
+  if (profileContainer) {
+    profileContainer.innerHTML = "";
+  }
 
-        profileImg.className = ("profile");
-        profileTitle.className = ("profile--title");
-        profileTextBox.className = ("profile--textBox");
-        profileText.className = ("profile--text");
+  for (let i = 0; i < profileList.length; i++) {
+    const profileImg = document.createElement("img");
+    const profileTitle = document.createElement("h2");
+    const profileTextBox = document.createElement("div");
+    const profileText = document.createElement("p");
+    //classes
+    profileImg.className = "profile";
+    profileTitle.className = "profile--title";
+    profileTextBox.className = "profile--textBox";
+    profileText.className = "profile--text";
+    //innerHTML
+    profileImg.src = dlcVideos[i].snippet.thumbnails.high.url;
+    //Append
+    profileContainer?.appendChild(profileImg);
+  }
+};
 
-        profileImg.src = dlcVideos[i].snippet.thumbnails.high.url;
-
-        profileContainer?.appendChild(profileImg);
-
-        navBar?.addEventListener("click",()=>{
-            profileList = [];
-            if(profileContainer){
-                profileContainer.innerHTML = "";
-            }
-            window.open("index.html", "_self");
-        })
-        
-    }
-}
-
+navBar?.addEventListener("click", () => {
+  profileList.pop();
+  window.open("index.html", "_self");
+  createHTMLProfile();
+});
